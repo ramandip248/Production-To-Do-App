afdfbede92ab9e0efc701f89aa28e2e37f2a2424
ramandip248 <89860029+ramandip248@users.noreply.github.com>
2023-07-31 13:49:57 +0530

    final code
    mern stack deployment

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const db = require('./config/mongoose');
+const taskRoutes = require('./routes/taskRoutes');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// // static files
+app.use(express.static(path.join(__dirname,'../frontend/todo_frontend_app/build')));
+
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname, '../frontend/todo_frontend_app/build/index.html'));
+})
+
+// Register task routes
+app.use('/api/tasks', taskRoutes);
+
+
+
+// Start the server
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
